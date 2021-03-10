994708bf8cec6f8d216da9ac048be63441db7cdf
226wyj <308033240@qq.com>
2021-03-10 12:21:40 +0800

    2021/3/10-12:21 one easy problem, 455.py, greedy

---
+++ Easy/455.py
@@ -0,0 +1,13 @@
+# 从饥饿度最小的孩子开始，分配最小的饼干
+# 若可以分配，则满足的孩子数量+1
+# 无论是否可以满足孩子，饼干数量都要不断+1，因为一个孩子只能至多吃一块饼干
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child, cookie = 0, 0
+        while (child < len(g) and cookie < len(s)):
+            if g[child] <= s[cookie]:
+                child += 1
+            cookie += 1
+        return child
