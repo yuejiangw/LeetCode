22c007e3cdcfd0ed43e8598adefa28d4c145d53a
226wyj <308033240@qq.com>
2021-03-13 19:52:39 +0800

    2021/3/13-19:52 452.py, medium, greedy

---
+++ Medium/452.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if len(points) == 0:
+            return 0
+        elif len(points) == 1:
+            return 1
+        else:
+            # 贪心算法，按照开始时间从小到大排序
+            points = sorted(points, key=lambda x: x[0])
+            arrow = 1
+            i = 0
+            min_end = points[i][1]
+            j = 1
+            while j < len(points):
+                min_end = min(min_end, points[j][1])
+                # 有重合部分，则arrow数量不变
+                if points[j][0] <= min_end:
+                    j += 1
+                # 重合气球数量达到最大值，重新开始计数
+                else:
+                    i = j
+                    min_end = points[i][1]
+                    j = i + 1
+                    arrow += 1
+            return arrow
