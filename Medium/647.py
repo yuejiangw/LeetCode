8064d87b9b34b7cead3c423cc9bedb5e33119866
226wyj <308033240@qq.com>
2021-04-07 13:24:39 +0800

    2021/4/7-13:24 another solution of 647

--- Medium/647.py
+++ Medium/647.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # 暴力方法
     def isPalindromic(self, s) -> bool:
         '''判断s是否是回文串'''
         return s == s[::-1]
@@ -15,3 +17,21 @@ class Solution:
         substrings = self.getSubstrings(s)
         result = list(map(self.isPalindromic, substrings))
         return result.count(True)
+
+    # 中心扩散
+    def countSubstrings(self, s: str) -> int:
+        def spread(left, right):
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
+
+        result = 0
+        for i in range(len(s)):
+            result += spread(i, i)
+
+        for i in range(len(s) - 1):
+            result += spread(i, i + 1)
+        return result
