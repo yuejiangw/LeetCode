6182669540f393144bdd6891b53eb5393deda441
226wyj <308033240@qq.com>
2021-04-17 15:55:18 +0800

    2021/4/17-15:55 665 greedy easy

---
+++ Easy/665.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                if i == 1 or nums[i] >= nums[i-2]:
+                    nums[i-1] = nums[i]
+                else:
+                    nums[i] = nums[i-1]
+                count += 1
+        return count <= 1
