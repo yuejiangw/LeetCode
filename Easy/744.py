748c94ba75c76a00fcb84a82908fa3925af91040
wuyuejiang <wuyuejiang@kuaishou.com>
2021-07-15 21:41:55 +0800

    744

--- Easy/744.py
+++ Easy/744.py
@@ -1,6 +1,11 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for l in letters:
-            if l > target:
-                return l
-        return letters[0]
+        l= 0
+        h = len(letters) - 1
+        while l <= h:
+            m = l + (h - l) // 2
+            if letters[m] <= target:
+                l = m + 1
+            else:
+                h = m - 1
+        return letters[l % len(letters)]
