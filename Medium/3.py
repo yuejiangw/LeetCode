35d8ea2dda24cf490bd5c484a26ad6b230db39bf
wuyuejiang <wuyuejiang@kuaishou.com>
2021-07-22 11:15:54 +0800

    3

--- Medium/3.py
+++ Medium/3.py
@@ -1,36 +1,14 @@
-"""
-    给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-"""
-
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        if s == "":
-            return 0
-        elif len(s) == 1:
-            return 1
-        else:
-            max_len = 0
-            sub_str = []
-            for i in range(len(s)):
-                # 到达末尾
-                if i == len(s) - 1:
-                    break
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result = 0
+        i, j = 0, 0
+        while i < len(s):
+            j = i + 1
+            while j < len(s):
+                if s[j] not in s[i:j]:
+                    j += 1
                 else:
-                    # 进行字串匹配
-                    sub_str.append(s[i])
-                    for j in range(i + 1, len(s)):
-                        if s[j] in sub_str:
-                            max_len = max(max_len, len(sub_str))
-                            break
-                        else:
-                            sub_str.append(s[j])
-                max_len = max(max_len, len(sub_str))
-                sub_str.clear()
-
-        # max_len = max(max_len, len(sub_str))
-        return max(1, max_len)
-
-
-str1 = "pwwkew"
-solution = Solution()
-print(solution.lengthOfLongestSubstring(str1))
+                    break
+            result = max(result, j - i)
+            i += 1
+        return result
