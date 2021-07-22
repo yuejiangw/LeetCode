85ab3a5706f2155dae84e1952dd098372213e8a0
wuyuejiang <wuyuejiang@kuaishou.com>
2021-07-22 14:25:36 +0800

    updae 21 and 70

--- Easy/21.py
+++ Easy/21.py
@@ -4,41 +4,20 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def getElements(self, l: ListNode) -> List[int]:
-        result = []
-        curr = l
-        while curr:
-            result.append(curr.val)
-            curr = curr.next
-        return result
-    
-    def getTail(self, l: ListNode) -> ListNode:
-        curr = l
-        while curr.next:
-            curr = curr.next
-        return curr
-
-    def getListNodeFromList(self, l: List[int]) -> ListNode:
-        if l == []:
-            return None
-        result = ListNode(None)
-        while l:
-            target = self.getTail(result)
-            target.val = l.pop(0)
-            if len(l) == 0:
-                break
-            target.next = ListNode(None)
-        return result
-
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        e1 = self.getElements(l1)
-        e2 = self.getElements(l2)
-        tmp = []
-        while e1 and e2:
-            if e1[0] <= e2[0]:
-                tmp.append(e1.pop(0))
+        head = ListNode()
+        curr = head
+        p1, p2 = l1, l2
+        while p1 is not None and p2 is not None:
+            if p1.val <= p2.val:
+                curr.next = p1
+                p1 = p1.next
             else:
-                tmp.append(e2.pop(0))
-        tmp += e1
-        tmp += e2
-        return self.getListNodeFromList(tmp)
+                curr.next = p2
+                p2 = p2.next
+            curr = curr.next
+        if p1 is not None:
+            curr.next = p1
+        if p2 is not None:
+            curr.next = p2
+        return head.next
