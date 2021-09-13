0b6ea7a1dd363351ef89076f8ca7f8d135fbe0ff
226wyj <308033240@qq.com>
2021-09-13 16:15:50 -0400

    update 203

--- Easy/203.py
+++ Easy/203.py
@@ -4,26 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
-    # 头插法建立链表
-    def createList(self, nums: List[int]) -> ListNode:
-        head = ListNode(None)
-        while nums:
-            newNode = ListNode(nums.pop(0))
-            if head.next == None:
-                head.next = newNode
-            else:
-                newNode.next = head.next
-                head.next = newNode
-        return head.next
-
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        curr = head
-        tmp = []
-        while curr:
-            tmp.append(curr.val)
-            curr = curr.next
-        while val in tmp:
-            tmp.remove(val)
-        # 因为采用了头插法，所以要倒序构建链表
-        return self.createList(tmp[::-1])
+        if not head:
+            return head
+        new_head = ListNode(None, head)
+        pre = new_head
+        curr = new_head.next
+        while curr is not None:
+            while curr is not None and curr.val == val:
+                pre.next = curr.next
+                curr = curr.next
+            pre = curr
+            if curr is not None:
+                curr = curr.next
+        return new_head.next 
