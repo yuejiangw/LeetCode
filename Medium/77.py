cf5e1f8ddb66de0ac5e3037e32e6c6183c5e8db3
226wyj <308033240@qq.com>
2021-09-22 11:08:54 -0400

    add comment for 77

--- Medium/77.py
+++ Medium/77.py
@@ -1,5 +1,10 @@
 from typing import List
 class Solution:
+    """
+    此题的坑在于第九行的path[:]，若不传副本则最后结果都是空
+    原因在于Python中参数传递是引用传递，最后的pop()方法会对
+    已经在result中的path也产生影响
+    """
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
         path = []
