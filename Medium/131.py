34115b9dfb6d2d135db4cd69bc6e3bc99849ba80
226wyj <308033240@qq.com>
2021-09-26 00:47:23 -0400

    add notations of 131

--- Medium/131.py
+++ Medium/131.py
@@ -10,12 +10,14 @@ class Solution:
                 res.append(sub_string[:])
                 return
             for i in range(start_index, len(s)):
+                # 判断是否是回文串
                 p = s[start_index: i + 1]
                 if p == p[::-1]:
                     sub_string.append(p)
                 else:
                     continue
-
+                
+                # 递归搜索后续的字符
                 backtracking(s, i + 1)
                 sub_string.pop()
         backtracking(s, 0)
