042cc4342e2a1317957003bc250e1a5371db1893
226wyj <308033240@qq.com>
2021-10-09 19:57:43 -0400

    bst的第k大结点

---
+++ 剑指Offer (2nd)/剑指Offer 54.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        res = []
+        
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+
+        return res[::-1][k - 1]
