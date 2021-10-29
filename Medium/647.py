6657570e91cf59cf1c0773ed1a6a6dbcea4dc671
226wyj <308033240@qq.com>
2021-10-29 12:26:05 -0400

    dp solution for 647

--- Medium/647.py
+++ Medium/647.py
@@ -35,3 +35,24 @@ class Solution:
         for i in range(len(s) - 1):
             result += spread(i, i + 1)
         return result
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        """动态规划"""
+        
+        l = len(s)
+        
+        # dp[i][j] 代表下标从i到j的s的回文串个数
+        dp = [[False for _ in range(l)] for _ in range(l)]
+
+        res = 0
+        for i in range(l - 1, -1, -1):
+            for j in range(i, l):
+                if s[i] == s[j]:
+                    if j - i <= 1:
+                        res += 1
+                        dp[i][j] = True
+                    elif dp[i + 1][j - 1]:
+                        dp[i][j] = True
+                        res += 1
+        return res
