6cbf2e7bf9575e9c94a2b9a87a56b4a9098d5ddd
226wyj <308033240@qq.com>
2021-11-03 11:20:08 -0400

    53, 376

--- Easy/53.py
+++ Easy/53.py
@@ -1,5 +1,36 @@
+from typing import List
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
             nums[i] = nums[i] + max(nums[i - 1], 0)
         return max(nums)
+
+
+class Solution:
+    """动态规划"""
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        res = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+            if dp[i] > res:
+                res = dp[i]
+        return res
+
+
+class Solution:
+    """贪心"""
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = float('-inf')
+        count = 0
+        for i in range(len(nums)):
+            count += nums[i]
+            if count > res:
+                res = count
+            if count <= 0:
+                count = 0
+        return res
---
+++ Medium/376.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        pre_diff = 0
+        curr_diff = 0
+        res = 1
+        for i in range(len(nums) - 1):
+            curr_diff = nums[i + 1] - nums[i]
+            if (curr_diff < 0 and pre_diff >= 0) or (curr_diff > 0 and pre_diff <= 0):
+                res += 1
+                pre_diff = curr_diff
+        return res
