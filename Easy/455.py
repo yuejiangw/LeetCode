697105a657f3a2e125d44ec154a6daf4d8a22f42
226wyj <308033240@qq.com>
2021-11-03 10:16:01 -0400

    455 review

--- Easy/455.py
+++ Easy/455.py
@@ -1,6 +1,9 @@
 # 从饥饿度最小的孩子开始，分配最小的饼干
 # 若可以分配，则满足的孩子数量+1
 # 无论是否可以满足孩子，饼干数量都要不断+1，因为一个孩子只能至多吃一块饼干
+
+from typing import List
+
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
@@ -11,3 +14,13 @@ class Solution:
                 child += 1
             cookie += 1
         return child
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()    # child
+        s.sort()    # cookie
+        idx = 0
+        for i in range(len(s)):
+            if idx < len(g) and s[i] >= g[idx]:
+                idx += 1
+        return idx
