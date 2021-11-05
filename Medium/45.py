34a6282b29a8dbbb35b66715ad9ff6c694e5fa68
226wyj <308033240@qq.com>
2021-11-05 15:27:09 -0400

    45

---
+++ Medium/45.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        res = 0
+        i = 0
+        curr_cover = 0  # 当前覆盖的最远距离下标
+        next_cover = 0  # 下一步覆盖的最远距离下标
+        while i < len(nums):
+            next_cover = max(next_cover, i + nums[i])
+            # 已经走到了当前覆盖的最远处
+            if i == curr_cover:
+                if curr_cover != len(nums) - 1:
+                    res += 1
+                    curr_cover = next_cover
+                    if next_cover >= len(nums) - 1:
+                        break
+                else:
+                    break
+            i += 1
+        return res
