6c4b1b4a916d824449c9b8e563a51a6bfa49257c
226wyj <308033240@qq.com>
2021-11-05 17:41:58 -0400

    406

--- Medium/406.py
+++ Medium/406.py
@@ -6,6 +6,7 @@ class Solution:
             return people
         
         # 贪心，h按照从高到低的顺序排序，k按从低到高排序
+        # lambda 返回的是一个元组，当-x[0]相同时，再根据x[1]进行排序
         people = sorted(people, key=lambda x: (-x[0], x[1]))
         
         result = []
