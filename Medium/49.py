166f65cfeda63657cfc284d84b8600b761cc1fb9
226wyj <308033240@qq.com>
2021-11-15 00:34:59 -0500

    49

---
+++ Medium/49.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1:
+            return [strs]
+        sorted_strs = {k: sorted(v) for k, v in enumerate(strs)}
+        sorted_strs = sorted(sorted_strs.items(), key=lambda x: x[1])
+        res = []
+        group = [strs[sorted_strs[0][0]]]
+        for i in range(1, len(sorted_strs)):
+            if sorted_strs[i][1] == sorted_strs[i - 1][1]:
+                group.append(strs[sorted_strs[i][0]])
+            else:
+                res.append(group[:])
+                group = [strs[sorted_strs[i][0]]]
+        res.append(group)
+        return res
