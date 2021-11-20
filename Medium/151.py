c8fa5549ef886371933dd0363162dca92244fb1e
226wyj <308033240@qq.com>
2021-11-19 22:37:54 -0500

    151

--- Medium/151.py
+++ Medium/151.py
@@ -1,26 +1,28 @@
-"""
-    题目描述：给定一个字符串，逐个翻转字符串中的每个单词。
-    无空格字符构成一个单词。
-    输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
-    如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
-"""
-
-class Solution():
-    def reverseWords(self, str):
-        str2list = str.strip().split(' ')
-        result = []
-        for word in str2list:
-            if word != '':
-                result.append(word)
-        return ' '.join(result[::-1])
-
-
-str1 = "the sky is blue"
-str2 = "  hello world!  "
-str3 = "a good    example"
-
-solution = Solution()
-result1 = solution.reverseWords(str1)
-result2 = solution.reverseWords(str2)
-result3 = solution.reverseWords(str3)
-print(result1 + '\n' + result2 + '\n' + result3)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        """正常做法，不调用任何api"""
+        # 去掉首尾空格
+        i, j = 0, len(s) - 1
+        while i < j and s[i] == ' ':
+            i += 1
+        while i < j and s[j] == ' ':
+            j -= 1
+        s = s[i: j + 1]
+        # 去掉单词中间多余空格
+        tmp = []
+        m, n = 0, 0
+        while n < len(s):
+            if s[n] != ' ':
+                n += 1
+            else:
+                tmp.append(s[m: n])
+                while n < len(s) and s[n] == ' ':
+                    n += 1
+                m = n
+        tmp.append(s[m:])
+        p, q = 0, len(tmp) - 1
+        while p < q:
+            tmp[p], tmp[q] = tmp[q], tmp[p]
+            p += 1
+            q -= 1
+        return ' '.join(tmp)
