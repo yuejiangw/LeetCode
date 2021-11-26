ef0bdab05b02915648463a8c0bddd072b6a1ac68
226wyj <308033240@qq.com>
2021-11-26 11:21:57 -0500

    update 232

--- Easy/232.py
+++ Easy/232.py
@@ -1,47 +1,26 @@
+from collections import deque
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.stack_in = []
-        self.stack_out = []
-
+        self.stack_in = deque()
+        self.stack_out = deque()
 
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
         self.stack_in.append(x)
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
-        while self.stack_in:
-            self.stack_out.append(self.stack_in.pop(-1))
-        target = self.stack_out.pop(-1)
-        while self.stack_out:
-            self.stack_in.append(self.stack_out.pop(-1))
-        return target
+        if len(self.stack_out) == 0:
+            while len(self.stack_in) > 0:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out.pop()
 
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        while self.stack_in:
-            self.stack_out.append(self.stack_in.pop(-1))
-        target = self.stack_out[-1]
-        while self.stack_out:
-            self.stack_in.append(self.stack_out.pop(-1))
-        return target
+        res = self.pop()
+        self.stack_out.append(res)
+        return res
 
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        return self.stack_in == []
-
+        return len(self.stack_in) == 0 and len(self.stack_out) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
