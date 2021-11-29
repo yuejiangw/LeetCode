247ef1d4a3cac05264cae3f9e824399b91c3a8b7
226wyj <308033240@qq.com>
2021-11-29 00:31:28 -0500

    973

---
+++ Medium/973.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points = sorted(points, key=lambda x: pow(x[0], 2) + pow(x[1], 2))
+        return points[:k]
