0b2db60944f519eb8eb63ad9b822ba49211ba0a6
226wyj <308033240@qq.com>
2021-12-16 22:56:05 -0500

    1518

---
+++ Easy/1518.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        res = numBottles
+
+        while numBottles >= numExchange:
+            res += numBottles // numExchange
+            numBottles = numBottles // numExchange + numBottles % numExchange
+        
+        return res
