7c0f33e018164da592f7fcda8a84f371bb201d2a
226wyj <308033240@qq.com>
2022-01-03 11:44:44 -0500

    update 73 O(1) space complexity solution

--- Medium/73.py
+++ Medium/73.py
@@ -27,3 +27,35 @@ class Solution:
             for i in range(row_len):
                 matrix[i][c] = 0
 
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # use the first row and the first column as the mark
+        # so we only need two other flag
+        # space complexity: O(1)
+        row = len(matrix)
+        col = len(matrix[0])
+
+        flag_row = any(matrix[0][j] == 0 for j in range(col))
+        flag_col = any(matrix[i][0] == 0 for i in range(row))
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if flag_row:
+            for j in range(col):
+                matrix[0][j] = 0
+
+        if flag_col:
+            for i in range(row):
+                matrix[i][0] = 0
