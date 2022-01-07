e9d238df5c308de3aee9d0ac25472a4f3131a879
226wyj <308033240@qq.com>
2022-01-07 17:20:34 -0500

    update O(1) space complexity solution for fib

--- Easy/509.py
+++ Easy/509.py
@@ -1,6 +1,22 @@
 class Solution:
+    """ 普通递归 """
     def fib(self, n: int) -> int:
         fibs = [0, 1]
         for i in range(2, n + 1):
             fibs.append(fibs[i - 1] + fibs[i - 2])
         return fibs[n]
+
+
+class Solution:
+    """ 迭代，空间复杂度 O(1) """
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        pre_1, pre_2 = 0, 1
+        for i in range(2, n):
+            tmp = pre_1
+            pre_1 = pre_2
+            pre_2 += tmp
+        return pre_1 + pre_2
--- Easy/70.py
+++ Easy/70.py
@@ -9,3 +9,17 @@ class Solution:
         for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]
+
+class Solution:
+    """ O(1) 空间复杂度 """
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        pre_1, pre_2 = 1, 2
+        for i in range(3, n):
+            tmp = pre_1
+            pre_1 = pre_2
+            pre_2 += tmp
+        return pre_1 + pre_2
