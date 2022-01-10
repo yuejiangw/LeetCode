63508f47dac7cc3610bc0c470b491a57e9073c83
226wyj <308033240@qq.com>
2022-01-09 19:34:30 -0500

    1614

--- Easy/1614.py
+++ Easy/1614.py
@@ -2,7 +2,6 @@ class Solution:
     def maxDepth(self, s: str) -> int:
         max_depth = 0
         res = 0
-        stack = []
         for char in s:
             if char == '(':
                 max_depth += 1
