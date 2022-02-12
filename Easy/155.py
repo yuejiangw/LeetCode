e930d4dbce3f3826e44af2afedf616b6505ed800
226wyj <308033240@qq.com>
2022-02-12 15:43:12 -0500

    update 155

--- Easy/155.py
+++ Easy/155.py
@@ -1,22 +1,26 @@
 class MinStack:
-
+    """
+    空间换时间, 每次入栈一个元组, 表示当前元素的 val 和到当前元素为止的最小值
+    这样可以保证 getMin 的时间复杂度为 O(1)
+    """
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.data = []
+        # Each element in the stack is a typle, represents for (val, min).
+        self.stack = []
 
     def push(self, val: int) -> None:
-        self.data.append(val)
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(self.stack[-1][1], val)))
 
     def pop(self) -> None:
-        self.data.pop(-1)
+        self.stack.pop()
 
     def top(self) -> int:
-        return self.data[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return min(self.data)
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
--- Medium/50.py
+++ Medium/50.py
@@ -1,3 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        def pow(x, n):
+            if n == 1:
+                return x
+            elif n % 2 == 0:
+                tmp = pow(x, n // 2)
+                return tmp * tmp
+            else:
+                tmp = pow(x, n // 2)
+                return tmp * tmp * x
+
         return pow(x, n)
