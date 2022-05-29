13a8c77ee4421fe87c381fb4fc06837c7a084a6b
226wyj <308033240@qq.com>
2022-05-29 19:44:14 -0400

    two hard

---
+++ Hard/329.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # 记忆化搜索, 不然会超时
+        # T: O(mn)
+        # S: O(mn)
+        m, n = len(matrix), len(matrix[0])
+        lookup = [[0] * n for _ in range(m)]
+
+        def dfs(i, j):
+            if lookup[i][j] != 0:
+                return lookup[i][j]
+            res = 1
+            for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[i][j]:
+                    res = max(res, 1 + dfs(x, y))
+            lookup[i][j] = res
+            return lookup[i][j]
+
+        return max(dfs(i, j) for i in range(m) for j in range(n))
