5030802d303e09d4702fc2208c37ce70f8d4e906
Yuejiang Wu <yuejiangw@indeed.com>
2024-03-27 17:33:46 -0700

    two medium

--- Medium/18.py
+++ Medium/18.py
@@ -44,3 +44,46 @@ class Solution:
             a += 1
         return res
     
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        """通用解法"""
+        def nSum(nums, n, start, target):
+            res = []
+            if n < 2:
+                return res
+            if n == 2:
+                i, j = start, len(nums) - 1
+                while i < j:
+                    curr = nums[i] + nums[j]
+                    left, right = nums[i], nums[j]
+                    if curr < target:
+                        # 去重
+                        while i < j and nums[i] == left:
+                            i += 1
+                    elif curr > target:
+                        # 去重
+                        while i < j and nums[j] == right:
+                            j -= 1
+                    else:
+                        res.append([left, right])
+                        # 去重
+                        while i < j and nums[i] == left:
+                            i += 1
+                        while i < j and nums[j] == right:
+                            j -= 1
+                return res
+            else:
+                i = start
+                while i < len(nums):
+                    tmp = nSum(nums, n - 1, i + 1, target - nums[i])
+                    for tmp_res in tmp:
+                        tmp_res.append(nums[i])
+                        res.append(tmp_res)
+                    # 去重
+                    while i < len(nums) - 1 and nums[i + 1] == nums[i]:
+                        i += 1
+                    i += 1
+            return res
+        
+        nums.sort()
+        return nSum(nums, 4, 0, target)
