ba37edc108dcb1072ca0122a57f29ab104ba46cc
Yuejiang Wu <yuejiangw@indeed.com>
2024-05-15 20:33:51 -0700

    递归法

--- Category/Binary Tree/Medium/513.py
+++ Category/Binary Tree/Medium/513.py
@@ -20,3 +20,26 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return res
+    
+class Solution:
+    res = None
+    max_depth = float('-inf')
+    
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        # 递归法, 深度最大的叶子节点一定是最后一行, 第一个遇到的深度最大的节点一定是最底层最左边的节点
+        def traversal(root, depth):
+            if not root.left and not root.right:
+                if depth > self.max_depth:
+                    self.max_depth = depth
+                    self.res = root.val
+            if root.left:
+                depth += 1
+                traversal(root.left, depth)    
+                depth -= 1
+            if root.right:
+                depth += 1
+                traversal(root.right, depth)
+                depth -= 1
+        
+        traversal(root, 0)
+        return self.res
