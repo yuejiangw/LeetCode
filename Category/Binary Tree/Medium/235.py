bb491fc6cac35a9fe671a8b81a90c6da369f9528
Yuejiang Wu <yuejiangw@indeed.com>
2024-05-17 14:14:18 -0700

    tree

--- Category/Binary Tree/Medium/235.py
+++ Category/Binary Tree/Medium/235.py
@@ -13,3 +13,14 @@ class Solution:
             return self.lowestCommonAncestor(root.right, p, q)
         else:
             return root
+
+# 迭代
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
--- Category/Binary Tree/Medium/450.py
+++ Category/Binary Tree/Medium/450.py
@@ -6,26 +6,33 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        # 未找到删除节点直接返回空
         if not root:
-            return None
+            return root
         if root.val == key:
-            if root.left is None:
+            # 左右都为空则返回空
+            if not root.left and not root.right:
+                return None
+            # 左子树为空则返回右子树
+            elif not root.left:
                 return root.right
-            elif root.right is None:
+            # 右子树为空则返回左子树
+            elif not root.right:
                 return root.left
+            # 左右子树都不为空则把左子树挂在右子树中最小节点下左子树的位置
+            # 右子树为新的根节点
             else:
                 curr = root.right
+                # 找最小
                 while curr.left:
                     curr = curr.left
                 curr.left = root.left
-                root = root.right
-                return root
+                return root.right
         if root.val > key:
             root.left = self.deleteNode(root.left, key)
         if root.val < key:
             root.right = self.deleteNode(root.right, key)
         return root
-
 
 class Solution:
     def find_min(self, root) -> TreeNode:
