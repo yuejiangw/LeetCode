37655efcf0d4518a033860e1422ae2e7b518a4ec
Yuejiang Wu <yuejiangw@indeed.com>
2024-05-20 20:18:39 -0700

    backtracking

--- Category/Backtracking/Medium/491.py
+++ Category/Backtracking/Medium/491.py
@@ -7,7 +7,7 @@ class Solution:
         def backtracking(nums, start_index):
             if len(path) > 1:
                 res.append(path[:])
-            
+            # 去重条件：同一父节点下同层上使用过的元素就不能再使用了
             used = set()
             for i in range(start_index, len(nums)):
                 if (len(path) > 0 and nums[i] < path[-1]) or nums[i] in used:
--- Category/Backtracking/Medium/90.py
+++ Category/Backtracking/Medium/90.py
@@ -1,6 +1,25 @@
 from typing import List
 
 class Solution:
+    # 不借助 used 数组来去重
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = []
+
+        def backtracking(nums, start):
+            res.append(path[:])
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                path.append(nums[i])
+                backtracking(nums, i + 1)
+                path.pop()
+        
+        # 为了去重，先排序
+        backtracking(sorted(nums), 0)
+        return res
+
+class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
@@ -12,6 +31,7 @@ class Solution:
             if start == len(nums):
                 return
             for i in range(start, len(nums)):
+                # 同一树层去重
                 if i > 0 and nums[i] == nums[i - 1] and used[i - 1] == False:
                     continue
                 path.append(nums[i])
