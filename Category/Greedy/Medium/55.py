b8380793b9c30231847569c3771b5896ccc90421
Yuejiang Wu <yuejiangw@indeed.com>
2024-05-26 23:07:44 -0700

    greedy

--- Category/Greedy/Medium/55.py
+++ Category/Greedy/Medium/55.py
@@ -2,10 +2,13 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        cover = 0
-        if len(nums) == 1:
+        # Greedy，假设我们在位置 i 起跳，可以选择的步长是 nums[i]
+        # 跳跃的终点要选择 nums[i + 1: i + nums[i] + 1] 中最大的那个数
+        # 本质上是判断跳跃覆盖范围是否可以到终点
+        if len(nums) <= 1:
             return True
         i = 0
+        cover = 0
         while i <= cover:
             cover = max(cover, i + nums[i])
             if cover >= len(nums) - 1:
--- Medium/45.py
+++ Medium/45.py
@@ -2,6 +2,30 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        # Greedy，假设当前数组下标为 i，则它可以 cover 的最大距离是 i + nums[i]
+        # 我们遍历 nums，每次跳跃要选 cover 最大的那个下标
+        if len(nums) == 1:
+            return 0
+        i = 0
+        cover = nums[0]
+        res = 0
+        while i < len(nums):
+            # 当前元素的 cover 可以覆盖数组末尾
+            if cover >= len(nums) - 1:
+                return res + 1
+            # 在 [i + 1, nums[i] + i] 中选 cover 最大的下标
+            next = i + 1
+            for j in range(i + 1, i + nums[i] + 1):
+                if j + nums[j] >= cover:
+                    next = j
+                    cover = j + nums[j]
+            i = next 
+            res += 1
+        return res
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return 0
         res = 0
