9a2a11eda39f3c18299e2525cafda1dad41d6f6f
Yuejiang Wu <yuejiangw@indeed.com>
2024-05-31 14:36:52 -0700

    two pointers

--- Category/Greedy/Medium/452.py
+++ Category/Greedy/Medium/452.py
@@ -2,6 +2,22 @@ from typing import List
 
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if len(points) == 1:
+            return 1
+        # 按照开始时间从小到大排序
+        points.sort(key=lambda x: x[0])
+        res = 1
+        for i in range(1, len(points)):
+            # 气球 i 和气球 i - 1 不挨着，需要额外的箭
+            if points[i][0] > points[i - 1][1]:
+                res += 1
+            else:
+                # 更新最小右边界
+                points[i][1] = min(points[i - 1][1], points[i][1])
+        return res
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
         if len(points) == 0:
             return 0
         elif len(points) == 1:
