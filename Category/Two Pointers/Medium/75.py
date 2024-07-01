360d90ba7328f1d18cd9f541511dc04ff5cb8051
Yuejiang Wu <yuejiangw@indeed.com>
2024-06-30 23:18:37 -0700

    75 - rainbow srot

--- Category/Two Pointers/Medium/75.py
+++ Category/Two Pointers/Medium/75.py
@@ -20,3 +20,42 @@ class Solution:
                 p0 += 1
                 p1 += 1
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Rainbow Sort
+        三个挡板
+        [0, i) 包含所有 0
+        [i, j) 包含所有 1
+        [j, k] 是探索区域
+        (k, len(nums) - 1] 包含所有 2
+        
+        三种情况：
+        1. nums[j] == 0: 和 i 换位置，i += 1, j += 1
+        2. nums[j] == 1, j += 1
+        3. nums[j] == 2: 和 k 换位置，k -= 1
+
+        example:
+               k
+        [0,0,1,1,2,2]
+             i     
+                 j
+        """
+
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        i = 0
+        j = 0
+        k = len(nums) - 1
+        while j <= k:
+            if nums[j] == 0:
+                swap(i, j)
+                i += 1
+                j += 1
+            elif nums[j] == 1:
+                j += 1
+            elif nums[j] == 2:
+                swap(j, k)
+                k -= 1
+            
---
+++ Medium/148.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        # split linked list into two parts
+        mid = self.getMid(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        # merge sort
+        return self.merge(left, right)
+    
+    def getMid(self, head):
+        pre = None
+        slow, fast = head, head
+        while fast and fast.next:
+            pre = slow
+            slow = slow.next
+            fast = fast.next.next
+        pre.next = None
+        return slow
+    
+    def merge(self, h1, h2):
+        p1, p2 = h1, h2
+        dummy = ListNode()
+        curr = dummy
+        while p1 and p2:
+            if p1.val <= p2.val:
+                curr.next = p1
+                p1 = p1.next
+            else:
+                curr.next = p2
+                p2 = p2.next
+            curr = curr.next
+        curr.next = p1 if p1 else p2
+        return dummy.next
