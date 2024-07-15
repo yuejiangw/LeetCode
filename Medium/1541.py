3d05e306b699f015db42d186abcf88e45bfeaf73
Yuejiang Wu <yuejiangw@indeed.com>
2024-07-14 22:55:48 -0700

    1541

--- Medium/1541.py
+++ Medium/1541.py
@@ -1,19 +1,19 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        res = 0     # 记录对左括号的需求数
-        need = 0    # 记录对右括号的需求数，根据 need 来判断是否需要插入
+        left_need = 0   # 左括号需求
+        right_need = 0  # 右括号需求
         for c in s:
             if c == '(':
-                # 一个左括号需要匹配两个右括号
-                need += 2
-                if need % 2 == 1:
-                    # 如果右括号的数量变成了奇数则需要插入一个右括号
-                    res += 1
-                    need -= 1
-            elif c == ')':
-                need -= 1
-                if need == -1:
-                    # 多出来一个右括号，需要插入一个左括号和一个右括号来匹配
-                    res += 1
-                    need = 1
-        return res + need
+                # 一个左括号需要两个右括号来匹配
+                right_need += 2
+                # 因为是 minimum insertion，所以尽可能把 right_need 转化为 left_need
+                if right_need % 2 == 1:
+                    left_need += 1
+                    right_need -= 1
+            else:
+                right_need -= 1
+                # 多出来一个右括号
+                if right_need == -1:
+                    right_need = 1
+                    left_need += 1
+        return left_need + right_need
