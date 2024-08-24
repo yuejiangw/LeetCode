c10f59d9aa9cddc5fe5039a7086480e5f60630f7
226wyj <308033240@qq.com>
2024-08-23 22:50:53 -0700

    1860

---
+++ Medium/1860.py
@@ -0,0 +1,11 @@
+class Solution:
+    def memLeak(self, memory1: int, memory2: int) -> List[int]:
+        # simulation
+        t = 1
+        while t <= max(memory1, memory2):
+            if memory1 < memory2:
+                memory2 -= t
+            else:
+                memory1 -= t
+            t += 1
+        return [t, memory1, memory2]
