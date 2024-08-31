f072ef8478b720452709987cee3dc9d2d79e067d
Yuejiang Wu <yuejiangw@indeed.com>
2024-08-31 11:46:13 -0700

    528

--- Category/Binary Search/Medium/528.py
+++ Category/Binary Search/Medium/528.py
@@ -1,33 +1,40 @@
 from typing import List
 import random
 
-
 class Solution:
+    '''
+    w = [1, 3, 5, 1]
+    n = 2
+
+    pre_sum = [1, 4, 9, 10]
+
+    w[0] = 1/10
+    w[1] = 3/10
+    w[2] = 5/10
+    w[3] = 1/10
+    '''
 
     def __init__(self, w: List[int]):
-        n = len(w)
-        self.pre_sum = [0] * (n + 1)
-        for i in range(1, n + 1):
-            self.pre_sum[i] = self.pre_sum[i - 1] + w[i - 1]
+        self.pre_sum = []
+        self.total_sum = 0
 
+        for weight in w:
+            self.total_sum += weight
+            self.pre_sum.append(self.total_sum)
+        
     def left_bound_search(self, nums, target):
-        if len(nums) == 0:
-            return -1
         i, j = 0, len(nums)
         while i < j:
             mid = i + (j - i) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
+            if nums[mid] >= target:
                 j = mid
             elif nums[mid] < target:
                 i = mid + 1
         return i
-    
-    def pickIndex(self) -> int:
-        target = random.randint(1, self.pre_sum[-1])
-        return self.left_bound_search(self.pre_sum, target) - 1
 
+    def pickIndex(self) -> int:
+        target = random.randint(1, self.total_sum)
+        return self.left_bound_search(self.pre_sum, target)
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
