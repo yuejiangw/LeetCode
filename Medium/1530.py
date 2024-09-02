b7c4ab247b89b42a7312cdf186ed1e84c34f8334
Yuejiang Wu <yuejiangw@indeed.com>
2024-09-02 14:34:30 -0700

    1530

--- Medium/1530.py
+++ Medium/1530.py
@@ -6,6 +6,21 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
+        '''
+        举例说明为什么在 traversal 函数中，当 root 恰好为 leaf node 的时候返回 [1]
+
+            1
+           / \
+           2  3
+          [1] [1]
+        
+        如上所示的二叉树，在 traversal(2) 和 traversal(3) 的时候，都返回 [1]，则在 traversal(1) 的时候
+        我们的 left 和 right 就分别是 [1] 和 [1]，直接代表了叶子节点到根节点之间的距离
+        这样一来，我们在计算 path 的时候就可以直接用 left 和 right 中的 value 相加
+
+        如果为了使 distance 的定义统一，在 root 即为 leaf 的时候返回 [0]，则最后在判断是否可以更新结果的时候，就要
+        写成 if x + y + 2 <= distance
+        '''
         # 遍历所有非叶节点 P，找到以 P 为最近公共祖先的所有叶子节点对并计算每一对之间的距离，统计距离不超过 distance 的数目
         # T: O(N * distance^2)
         # S: O(H * distance)
