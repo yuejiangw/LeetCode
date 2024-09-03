c8de8360e7e199f6e3441acfa1b452f0688dd7cd
Yuejiang Wu <yuejiangw@indeed.com>
2024-09-02 20:33:35 -0700

    92

---
+++ Medium/34.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def lowerBound(self, nums: List[int], target: int) -> int:
+        low = 0
+        up = len(nums)
+        while low < up:
+            mid = (low + up) // 2
+            if nums[mid] >= target:
+                up = mid
+            else:
+                low = mid + 1
+        return low
+    
+    def upperBound(self, nums: List[int], target: int) -> int:
+        low = 0
+        up = len(nums)
+        while low < up:
+            mid = (low + up) // 2
+            if nums[mid] > target:
+                up = mid
+            else:
+                low = mid + 1
+        return low
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0:
+            return [-1, -1]
+        else:
+            low = self.lowerBound(nums, target)
+            up = self.upperBound(nums, target) - 1
+            if low == len(nums) or nums[low] != target:
+                return [-1, -1]
+            return [low, up]        
--- Medium/92.py
+++ Medium/92.py
@@ -19,6 +19,39 @@ class Solution:
         return dummy_head.next
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        '''
+        left = 2, right = 4
+            pre   4 ->  3
+                        \
+        dummy -> 1     -> 2  -> (None)              5
+            reverse_pre                               cur
+        '''
+        dummy_head = ListNode(0, head)
+        pre = dummy_head
+        for _ in range(1, left):
+            pre = pre.next
+        # curr 所在位置即为 left 
+        curr = pre.next
+        reverse_pre = pre
+        pre = None
+        for _ in range(right - left + 1):
+            next_node = curr.next
+            curr.next = pre
+            pre = curr
+            curr = next_node
+        
+        reverse_pre.next.next = curr
+        reverse_pre.next = pre
+
+        return dummy_head.next
+
 
 class Solution:
     """ 递归 """
