c640c0af26d8aa5fd78d2a62fed71ba3bcc7aacd
Yuejiang Wu <yuejiangw@indeed.com>
2024-09-03 23:36:00 -0700

    165

--- Medium/165.py
+++ Medium/165.py
@@ -1,5 +1,23 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
+        # 更简洁的写法
+        version1 = version1.split('.')
+        version2 = version2.split('.')
+        
+        n = max(len(version1), len(version2))
+
+        for i in range(n):
+            s1 = version1[i] if i < len(version1) else '0'
+            s2 = version2[i] if i < len(version2) else '0'
+            if int(s1) < int(s2):
+                return -1
+            elif int(s1) > int(s2):
+                return 1
+        return 0
+
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
         v1, v2 = version1.split('.'), version2.split('.')
         i, j = 0, 0
         while i < len(v1) and j < len(v2):
