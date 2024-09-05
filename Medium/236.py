afcc4fe5544d79142b077cfe09e72e4b61ba88bb
Yuejiang Wu <yuejiangw@indeed.com>
2024-09-04 17:56:59 -0700

    236

---
+++ Medium/236.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == q or root == p or root is None:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left is not None and right is not None:
+            return root
+        if left is None:
+            return right
+        return left
