483b4d2be8a8156731c4170109abf6a16e6437b8
Yuejiang Wu <yuejiangw@indeed.com>
2024-12-15 14:06:17 -0800

    LRU cache，添加 design 版本

--- Medium/146.py
+++ Medium/146.py
@@ -1,6 +1,69 @@
 from collections import OrderedDict
 
 
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.pre = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        # 双向链表 + 哈希表
+        self.capacity = capacity
+        self.dic = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def add(self, node):
+        pre_end = self.tail.pre
+        pre_end.next = node
+        node.pre = pre_end
+        node.next = self.tail
+        self.tail.pre = node
+    
+    def remove(self, node):
+        node.pre.next = node.next
+        node.next.pre = node.pre
+
+    def make_recent(self, node):
+        # the most recent node is at the tail of the linked list
+        # in other words, if we want to remove a node when the cache is at its capacity,
+        # we should remove the one at the head
+        self.remove(node)
+        self.add(node)
+    
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        node = self.dic[key]
+        self.make_recent(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            node.val = value
+            self.make_recent(node)
+            return
+        
+        new_node = ListNode(key, value)
+        self.dic[key] = new_node
+        self.add(new_node)
+
+        if len(self.dic) > self.capacity:
+            node_to_remove = self.head.next
+            self.remove(node_to_remove)
+            del self.dic[node_to_remove.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
 class LRUCache:
     """
     要求 get 和 put 的时间复杂度都是 O(1)，则需要用 LinkedHashMap
