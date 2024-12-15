4155152a74876f56a2ef70add5ec01a69e488390
Yuejiang Wu <yuejiangw@indeed.com>
2024-12-14 17:29:53 -0800

    update 973

--- Medium/973.py
+++ Medium/973.py
@@ -1,5 +1,18 @@
 from typing import List
+from heapq import heappush, heappop
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points = sorted(points, key=lambda x: pow(x[0], 2) + pow(x[1], 2))
-        return points[:k]
+        # T: O(Nlogk), S:O(k)
+        def get_distance(point: List[int]):
+            x, y = point
+            return x * x + y * y
+        
+        heap = [] # max heap
+        for point in points:
+            distance = get_distance(point)
+            heappush(heap, [-distance, point])
+            if len(heap) > k:
+                heappop(heap)
+        
+        return [x[1] for x in heap]
